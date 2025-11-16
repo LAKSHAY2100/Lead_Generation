7e98086f96550c3f4bc92684574190ab8f163a3b
LAKSHAY2100 <lakshaybajaj210@gmail.com>
2025-11-16 13:44:37 +0530

    Job desc

--- src/lead_score_flow/types.py
+++ src/lead_score_flow/types.py
@@ -1,8 +1,28 @@
 from pydantic import BaseModel,Field
 
+class JobDescription(BaseModel):
+    title : str
+    description : str
+    skills : str
+
 class CandidateScore(BaseModel):
     id : int = Field("ID of the candidate")
     score : int = Field("Score of the candidate")
     reason : str = Field("Reason for the score you assigned")
 
-    
+
+class Candidate(BaseModel):
+    id: str
+    name : str
+    bio : str
+    email : str
+    skills : str
+
+class ScoredCandidates(BaseModel):
+    id: str
+    name : str
+    bio : str
+    email : str
+    skills : str
+    score : int
+    reason : str    
